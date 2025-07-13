4d7259fd87e0ac938ea5877c397956ac6b50ca79
Joao <joao.ntc@gmail.com>
2025-07-13 16:40:29 +1200

    Desafio React finalizado com contador funcional

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <h1>Meu primeiro desafio em React</h1>
       <p>React funcionando! 🚀</p>
+      <p>Você clicou {count} vezes.</p>
+      <button onClick={() => setCount(count + 1)}>
+        Clique aqui
+      </button>
     </div>
   );
 }
+
 export default App;
